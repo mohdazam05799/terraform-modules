202a8268794fe61ca5ec5f9682a5c296738b3982
mohdazam05799 <mohdazam05799@gmail.com>
2024-07-09 12:19:28 +0530

    Updated vpc module to create nat gateway dynamically

--- VPC/natgw.tf
+++ VPC/natgw.tf
@@ -1,13 +1,14 @@
-
 resource "aws_eip" "eip" {
   count  = var.nat_count
-  domain = "vpc"
-
+  vpc    = true  # Ensure the EIP is created in a VPC
 }
 
 resource "aws_nat_gateway" "ngw" {
   count         = var.nat_count
-  subnet_id     = aws_subnet.public_subnet[0].id
-  allocation_id = aws_eip.eip[0].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+  allocation_id = aws_eip.eip[count.index].id
 
+  tags = {
+    Name = "nat-gateway-${count.index + 1}"  # Example tag naming convention
+  }
 }
